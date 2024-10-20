aae4c0bc35f40790614e1e39aea347e2f66f66d1
孙楠 <sn01640106@alibaba-inc.com>
2024-10-20 16:43:34 +0800

    feat: test

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -51,6 +51,11 @@ const Login = () => {
     };
   }, []);
 
+  useEffect(() => {
+    console.log('test');
+    
+  }, [])
+
   const refreshCaptcha = () => {
     const imgCaptcha: any = document.getElementById('imgCaptcha');
     imgCaptcha.src = `/api/captcha?rad=${Math.random()}`;
